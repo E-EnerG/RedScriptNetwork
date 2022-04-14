4698238d3edeb9c6e10959105b3e5920f3713dcd
E-EnerG <93612716+E-EnerG@users.noreply.github.com>
2022-04-14 09:08:03 -0400

    The first maintenance change has occurred (Neatening some code)

--- main.ts
+++ main.ts
@@ -32,6 +32,111 @@ function Create_file (text: string) {
         blockSettings.writeString("main.folder.storage.viewer", "1111")
         currentsessionstorageapps += 1
         mainstorage += 1
+    } else if (text == "3") {
+        mySprite = sprites.create(img`
+            2 2 2 . . . . . . . . . . . . . 
+            2 2 2 2 5 5 5 5 5 5 5 5 5 1 4 . 
+            2 2 2 2 2 f f f f f f f 5 1 4 . 
+            . 2 2 2 2 2 5 5 5 5 5 5 5 1 4 . 
+            . 5 2 2 2 2 2 f f f f f 5 1 4 . 
+            . 5 5 2 2 2 2 2 5 5 5 5 5 1 4 . 
+            . 5 f f 2 2 2 2 2 f f f 5 1 4 . 
+            . 5 5 5 5 2 2 2 2 2 5 5 5 1 4 . 
+            . 5 f f f f 2 2 2 2 2 5 5 1 4 . 
+            . 5 5 5 5 5 5 2 2 2 2 2 5 1 4 . 
+            . 5 5 5 5 5 5 5 2 2 2 2 2 1 4 . 
+            . 5 5 5 5 5 5 5 5 2 2 2 2 2 4 . 
+            . 5 5 5 5 5 5 5 5 5 2 2 2 2 2 . 
+            . 5 5 5 5 5 5 5 5 5 f 2 2 2 2 2 
+            . 5 5 5 5 5 5 5 5 5 5 5 2 2 2 2 
+            . . . . . . . . . . . . . 2 2 2 
+            `, SpriteKind.MainSystemFile)
+        mySprite.setPosition(20, 25)
+        mySprite3 = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . 2 2 2 2 2 2 2 7 7 7 7 7 7 7 . 
+            . 2 1 1 1 1 1 1 1 1 1 1 1 1 7 . 
+            . 2 1 1 f f f f f f f f 1 1 7 . 
+            . 2 1 f 1 1 1 1 1 1 1 1 f 1 7 . 
+            . 2 1 1 1 1 1 1 1 1 1 1 f 1 7 . 
+            . 2 1 1 1 1 1 1 1 1 1 1 f 1 7 . 
+            . 2 1 1 1 1 1 1 1 1 1 1 f 1 7 . 
+            . 2 1 1 1 1 1 1 f f f f 1 1 7 . 
+            . 2 1 1 1 1 1 f 1 1 1 1 1 1 7 . 
+            . 2 1 1 1 1 1 f 1 1 1 1 1 1 7 . 
+            . 2 1 1 1 1 1 1 1 1 1 1 1 1 7 . 
+            . 2 1 1 1 1 1 f 1 1 1 1 1 1 7 . 
+            . 2 1 1 1 1 1 1 1 1 1 1 1 1 7 . 
+            . 2 2 2 2 2 2 2 7 7 7 7 7 7 7 . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.MainSystemFile)
+        mySprite3.setPosition(20, 57)
+        mySprite4 = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . a a a a a a a a a a a a a a . 
+            . a a a a a a a a a a a a a a . 
+            . a a a a a a a a a a a a a a . 
+            . a a a a a a a a a a a a a a . 
+            . a a a a a a a a a a a a a a . 
+            . a a a f f f f f f f f a a a . 
+            . a a a a f f f f f f a a a a . 
+            . a a a a a a a a a a a a a a . 
+            . a a a a a a a a a a a a a a . 
+            . a a a a a a a a a a a a a a . 
+            . a a a a a a a a a a a a a a . 
+            . a a a a a a a a a a a a a a . 
+            . a a a a a a a a a a f f f a . 
+            . a a a a a a a a a a a a a a . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.MainSystemFile)
+        mySprite4.setPosition(20, 89)
+        personalization = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . 2 2 4 4 5 5 7 7 8 8 a a 3 3 . 
+            . 2 2 4 4 5 5 7 7 8 8 a a 3 3 . 
+            . e e d d b b c c f f 1 1 2 2 . 
+            . e e d d b b c c f f 1 1 2 2 . 
+            . 4 4 5 5 7 7 8 8 a a 3 3 e e . 
+            . 4 4 5 5 7 7 8 8 a a 3 3 e e . 
+            . d d b b c c f f 1 1 2 2 4 4 . 
+            . d d b b c c f f 1 1 2 2 4 4 . 
+            . 5 5 7 7 8 8 a a 3 3 e e d d . 
+            . 5 5 7 7 8 8 a a 3 3 e e d d . 
+            . b b c c f f 1 1 2 2 4 4 5 5 . 
+            . b b c c f f 1 1 2 2 4 4 5 5 . 
+            . 7 7 8 8 a a 3 3 e e d d b b . 
+            . 7 7 8 8 a a 3 3 e e d d b b . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.MainSystemFile)
+        personalization.setPosition(70, 25)
+        mySprite.z = -15
+        mySprite3.z = -15
+        mySprite4.z = -15
+        personalization.z = -15
+    } else if (text == "4") {
+        tiles.setCurrentTilemap(tilemap`level4`)
+        scene.setBackgroundColor(6)
+    } else if (text == "5") {
+        textSprite = textsprite.create("Volume - " + convertToText(Volume) + "", 1, 15)
+        textSprite.setBorder(2, 1, 0)
+        textSprite.setPosition(100, 100)
+    } else if (text == "6") {
+        Volume = 50
+        BrightnessWhite = 255
+        BrightnessBlack = 0
+        dothis = 0
+    } else if (text == "7") {
+        mySprite2 = sprites.create(img`
+            f f f f f f f 
+            f f f f f f f 
+            f f f f f f f 
+            f f f f f f f 
+            f f f f f f f 
+            f f f f f f f 
+            f f f f f f f 
+            `, SpriteKind.Player)
+        controller.moveSprite(mySprite2)
+        mySprite2.z = 55
     }
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.MainSystemFile, function (sprite, otherSprite) {
@@ -77,114 +182,31 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, l
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     dothis = 0
 })
+function CreateFiles () {
+    Create_file("1")
+    Create_file("2")
+    Create_file("3")
+    Create_file("4")
+    Create_file("5")
+    Create_file("6")
+    Create_file("7")
+}
 let dothis2 = 0
-let mainstorage = 0
-let currentsessionstorageapps = 0
-let currentsessionstorageuser = 0
-let currentsessionstoragecboy = 0
+let mySprite2: Sprite = null
+let BrightnessBlack = 0
+let BrightnessWhite = 0
+let Volume = 0
+let textSprite: TextSprite = null
 let personalization: Sprite = null
 let mySprite4: Sprite = null
 let mySprite3: Sprite = null
 let mySprite: Sprite = null
+let mainstorage = 0
+let currentsessionstorageapps = 0
+let currentsessionstorageuser = 0
+let currentsessionstoragecboy = 0
 let dothis = 0
-let Volume = 50
-let BrightnessWhite = 255
-let BrightnessBlack = 0
-dothis = 0
-let textSprite = textsprite.create("Volume - " + convertToText(Volume) + "", 1, 15)
-textSprite.setBorder(1, 1, 0)
-textSprite.setPosition(100, 100)
-tiles.setCurrentTilemap(tilemap`level4`)
-Create_file("1")
-Create_file("2")
-scene.setBackgroundColor(6)
-mySprite = sprites.create(img`
-    2 2 2 . . . . . . . . . . . . . 
-    2 2 2 2 5 5 5 5 5 5 5 5 5 1 4 . 
-    2 2 2 2 2 f f f f f f f 5 1 4 . 
-    . 2 2 2 2 2 5 5 5 5 5 5 5 1 4 . 
-    . 5 2 2 2 2 2 f f f f f 5 1 4 . 
-    . 5 5 2 2 2 2 2 5 5 5 5 5 1 4 . 
-    . 5 f f 2 2 2 2 2 f f f 5 1 4 . 
-    . 5 5 5 5 2 2 2 2 2 5 5 5 1 4 . 
-    . 5 f f f f 2 2 2 2 2 5 5 1 4 . 
-    . 5 5 5 5 5 5 2 2 2 2 2 5 1 4 . 
-    . 5 5 5 5 5 5 5 2 2 2 2 2 1 4 . 
-    . 5 5 5 5 5 5 5 5 2 2 2 2 2 4 . 
-    . 5 5 5 5 5 5 5 5 5 2 2 2 2 2 . 
-    . 5 5 5 5 5 5 5 5 5 f 2 2 2 2 2 
-    . 5 5 5 5 5 5 5 5 5 5 5 2 2 2 2 
-    . . . . . . . . . . . . . 2 2 2 
-    `, SpriteKind.MainSystemFile)
-mySprite.setPosition(20, 25)
-let mySprite2 = sprites.create(img`
-    f f f f f f f 
-    f f f f f f f 
-    f f f f f f f 
-    f f f f f f f 
-    f f f f f f f 
-    f f f f f f f 
-    f f f f f f f 
-    `, SpriteKind.Player)
-controller.moveSprite(mySprite2)
-mySprite3 = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . 2 2 2 2 2 2 2 7 7 7 7 7 7 7 . 
-    . 2 1 1 1 1 1 1 1 1 1 1 1 1 7 . 
-    . 2 1 1 f f f f f f f f 1 1 7 . 
-    . 2 1 f 1 1 1 1 1 1 1 1 f 1 7 . 
-    . 2 1 1 1 1 1 1 1 1 1 1 f 1 7 . 
-    . 2 1 1 1 1 1 1 1 1 1 1 f 1 7 . 
-    . 2 1 1 1 1 1 1 1 1 1 1 f 1 7 . 
-    . 2 1 1 1 1 1 1 f f f f 1 1 7 . 
-    . 2 1 1 1 1 1 f 1 1 1 1 1 1 7 . 
-    . 2 1 1 1 1 1 f 1 1 1 1 1 1 7 . 
-    . 2 1 1 1 1 1 1 1 1 1 1 1 1 7 . 
-    . 2 1 1 1 1 1 f 1 1 1 1 1 1 7 . 
-    . 2 1 1 1 1 1 1 1 1 1 1 1 1 7 . 
-    . 2 2 2 2 2 2 2 7 7 7 7 7 7 7 . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.MainSystemFile)
-mySprite3.setPosition(20, 57)
-mySprite4 = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . a a a a a a a a a a a a a a . 
-    . a a a a a a a a a a a a a a . 
-    . a a a a a a a a a a a a a a . 
-    . a a a a a a a a a a a a a a . 
-    . a a a a a a a a a a a a a a . 
-    . a a a f f f f f f f f a a a . 
-    . a a a a f f f f f f a a a a . 
-    . a a a a a a a a a a a a a a . 
-    . a a a a a a a a a a a a a a . 
-    . a a a a a a a a a a a a a a . 
-    . a a a a a a a a a a a a a a . 
-    . a a a a a a a a a a a a a a . 
-    . a a a a a a a a a a f f f a . 
-    . a a a a a a a a a a a a a a . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.MainSystemFile)
-mySprite4.setPosition(20, 89)
-personalization = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . 2 2 4 4 5 5 7 7 8 8 a a 3 3 . 
-    . 2 2 4 4 5 5 7 7 8 8 a a 3 3 . 
-    . e e d d b b c c f f 1 1 2 2 . 
-    . e e d d b b c c f f 1 1 2 2 . 
-    . 4 4 5 5 7 7 8 8 a a 3 3 e e . 
-    . 4 4 5 5 7 7 8 8 a a 3 3 e e . 
-    . d d b b c c f f 1 1 2 2 4 4 . 
-    . d d b b c c f f 1 1 2 2 4 4 . 
-    . 5 5 7 7 8 8 a a 3 3 e e d d . 
-    . 5 5 7 7 8 8 a a 3 3 e e d d . 
-    . b b c c f f 1 1 2 2 4 4 5 5 . 
-    . b b c c f f 1 1 2 2 4 4 5 5 . 
-    . 7 7 8 8 a a 3 3 e e d d b b . 
-    . 7 7 8 8 a a 3 3 e e d d b b . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.MainSystemFile)
-personalization.setPosition(70, 25)
-mySprite2.z = 55
+CreateFiles()
 game.onUpdate(function () {
     console.logValue("Total Settings", blockSettings.readNumber("main.setting.storage"))
     console.logValue("Total Settings (in Cboy)", blockSettings.readNumber("cboy.setting.storage"))
